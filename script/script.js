3600749c58d723abdd8e725cabcc5469fea28102
unknown <md031194@gmail.com>
2025-02-12 00:50:43 -0300

    feat: capture inputs and selects with js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
-const input = document.querySelector("name")
+const input = document.querySelector("input")
+input.addEventListener("input", (event)=>{
+    console.log(input.value)
+})
+
+const select = document.querySelector("select")
+select.addEventListener("change", ()=>{
+    console.log(select.value)
+})
+
+
+function converMoeda (inputValue, selectValue){
+const result = (inputValue * selectValue)
+return result
+
+}
