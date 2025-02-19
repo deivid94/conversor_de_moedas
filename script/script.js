3cda344c50fcb8b869ea875475e01d678755c785
unknown <md031194@gmail.com>
2025-02-19 00:32:52 -0300

    fix: switch coin symbol with value

--- script/script.js
+++ script/script.js
@@ -1,18 +1,45 @@
-function convertMoeda() {
+const USD = 4.87
+const EUR = 5.00
+const GBP = 7.20
+
 
 const input = document.querySelector("input")
-input.addEventListener("input", (event)=>{
-    return input.value
-})
-
 const select = document.querySelector("select")
-select.addEventListener("change", ()=>{
-    return select.value
+const form = document.querySelector("form")
+const resultado = document.getElementById("resultado")
+const calculo = document.getElementById("calculo")
+
+
+input.addEventListener("input", ()=>{
+    const CharacterRegex = /\D+/g
+    input.value = input.value.replace(CharacterRegex, "")
+    
+    
 })
 
-const result = parseInt((input.value * select.value))
-document.getElementById("resultado")
-.innerHTML = result
+form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    switch (select.value){
+        case "USD" :
+            ConvertMoeda(input.value, USD, "US$")
+            break
+        case "EUR" :
+            ConvertMoeda(input.value, EUR,"€")
+            break
+        case "GBP" :
+            ConvertMoeda(input.value, GBP,"£" )    
+            break
 
+    }   
+    
+})
+
+
+function ConvertMoeda (input, price, coin) {
+    let result = (input*price) +" " +coin
+    resultado.innerText = result
+    
+    
 }
+
 
